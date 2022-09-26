beeaa398088b345233e7cf81114f595326e84513
HandsomeStranger <manuxin2002@bk.ru>
2022-09-26 14:37:26 +0300

    Home work 5

--- client.py
+++ client.py
@@ -2,20 +2,24 @@ import json
 import sys
 import socket
 import time
+import logging
+import log.client_log_config 
 
 from utils import load_configs, send_message, get_message
 
 CONFIGS = dict()
 
+logger = logging.getLogger('client')
 
-def create_presence_message(account_name, CONFIGS):
+def create_presence_message(account_name, action, CONFIGS):
     message = {
-        CONFIGS.get('ACTION'): CONFIGS.get('PRESENCE'),
+        CONFIGS.get('ACTION'): CONFIGS.get(f'{action.upper()}'),
         CONFIGS.get('TIME'): time.time(),
         CONFIGS.get('USER'): {
             CONFIGS.get('ACCOUNT_NAME'): account_name
         }
     }
+    logger.info(f'created {action} message')
     return message
 
 
@@ -24,6 +28,7 @@ def handle_response(message, CONFIGS):
         if message[CONFIGS.get('RESPONSE')] == 200:
             return '200 : OK'
         return f'400 : {message[CONFIGS.get("ERROR")]}'
+    logger.error(f'raised ValueError in HandleResponse()')
     raise ValueError
 
 
@@ -40,19 +45,27 @@ def main():
         server_port = CONFIGS.get('DEFAULT_PORT')
     except ValueError:
         print('Порт должен быть указан в пределах от 1024 до 65535')
+        logger.warning('Указан порт, не входящий в диапазон от 1024 до 65535')
         sys.exit(1)
 
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     transport.connect((server_address, server_port))
-    presence_message = create_presence_message('Guest', CONFIGS)
+    presence_message = create_presence_message('Guest','presence', CONFIGS)
     send_message(transport, presence_message, CONFIGS)
     try:
         response = get_message(transport, CONFIGS)
         hanlded_response = handle_response(response, CONFIGS)
         print(f'Ответ от сервера: {response}')
         print(hanlded_response)
+        exit_message = create_presence_message('Guest','exit', CONFIGS)
+        send_message(transport, exit_message, CONFIGS)
+        response = get_message(transport, CONFIGS)
+        hanlded_response = handle_response(response, CONFIGS)
+        print(f'Ответ от сервера: {response}')
+        print(hanlded_response)
     except (ValueError, json.JSONDecodeError):
         print('Ошибка декодирования сообщения')
+        logger.error('Ошибка декодирования сообщения от сервера у клиента')
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,19 +1,27 @@
+from cmath import log
 import json
 import sys
 import socket
+import logging
 
 from utils import load_configs, get_message, send_message
 
+
 CONFIGS = dict()
+
+logger = logging.getLogger('server')
 
 
 def handle_message(message, CONFIGS):
     if CONFIGS.get('ACTION') in message \
-            and message[CONFIGS.get('ACTION')] == CONFIGS.get('PRESENCE') \
+            and (message[CONFIGS.get('ACTION')] == CONFIGS.get('PRESENCE')
+                 or message[CONFIGS.get('ACTION')] == CONFIGS.get('EXIT')) \
             and CONFIGS.get('TIME') in message \
             and CONFIGS.get('USER') in message \
             and message[CONFIGS.get('USER')][CONFIGS.get('ACCOUNT_NAME')] == 'Guest':
+        logger.info('Response message to client created.Response code:200')
         return {CONFIGS.get('RESPONSE'): 200}
+    logger.error('Response message for client was created.Response code: 400')
     return {
         CONFIGS.get('RESPONSE'): 400,
         CONFIGS.get('ERROR'): 'Bad Request'
@@ -31,11 +39,10 @@ def main():
         if not 65535 >= listen_port >= 1024:
             raise ValueError
     except IndexError:
-        print('После -\'p\' необходимо указать порт')
+        logger.error('После -\'p\' необходимо указать порт')
         sys.exit(1)
     except ValueError:
-        print(
-            'Порт должен быть указан в пределах от 1024 до 65535')
+        logger.error('Порт должен быть указан в пределах от 1024 до 65535')
         sys.exit(1)
 
     try:
@@ -45,25 +52,31 @@ def main():
             listen_address = ''
 
     except IndexError:
-        print(
-            'После \'a\'- необходимо указать адрес для ')
+        logger.error('После \'a\'- необходимо указать адрес для подключения')
         sys.exit(1)
 
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     transport.bind((listen_address, listen_port))
 
     transport.listen(CONFIGS.get('MAX_CONNECTIONS'))
-
-    while True:
-        client, client_address = transport.accept()
+    client, client_address = transport.accept()
+    message = get_message(client, CONFIGS)
+    while message.get('action') != 'exit':
         try:
-            message = get_message(client, CONFIGS)
             response = handle_message(message, CONFIGS)
+            print(response)
+            print(message)
             send_message(client, response, CONFIGS)
-            client.close()
+            message = get_message(client, CONFIGS)
+            logger.info('Сервер принял сообщение от клиента')
         except (ValueError, json.JSONDecodeError):
-            print('Принято некорретное сообщение от клиента')
+            logger.fatal('Принято некорретное сообщение от клиента')
             client.close()
+    response = handle_message(message, CONFIGS)
+    print(response)
+    print(message)
+    send_message(client, response, CONFIGS)
+    client.close()
 
 
 if __name__ == '__main__':
